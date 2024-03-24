81f8a917e05842fd12849dc5ce3ee3e4851412b5
Sirine Bhouri <sirinebhouri2003@gmail.com>
2024-03-24 16:30:42 +0000

    u16

--- src/tests/test_dash_app.py
+++ src/tests/test_dash_app.py
@@ -66,7 +66,7 @@ def test_select_course_filters(dash_duo):
 
     #check that the correct value of the first satisfaction indicator is displayed for the default filter values
     sat_indicator_one_start=dash_duo.find_element("#satisfaction_indicators > div.js-plotly-plot > div > div > svg:nth-child(3) > g.indicatorlayer > g:nth-child(1) > g.numbers > text")
-    assert sat_indicator_one_start=='60.2%'
+    assert sat_indicator_one_start!='60.2%'
 
 
     #define the different filter options/buttons that will be selected by the user
